7e9a4f6ed355f5c939c9b2016b9002780eaf3fa7
Rob Zienert <rob@robzienert.com>
2011-01-16 16:59:00 -0500

    working on getting schema creation to work

---
+++ Entity/CalendarRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bundle\CalendarBundle\Entity;
+
+use Bundle\CalendarBundle\Model\CalendarRepositoryInterface;
+
+class CalendarRepository extends ObjectRepository implements CalendarRepositoryInterface
+{
+}
---
+++ Entity/ObjectRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bundle\CalendarBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+use Bundle\CalendarBundle\RepositoryInterface;
+
+abstract class ObjectRepository extends EntityRepository implements RepositoryInterface
+{
+    /**
+     * @see RepositoryInterface::getObjectManager
+     */
+    public function getObjectManager()
+    {
+        return $this->getEntityManager();
+    }
+
+    /**
+     * @see RepositoryInterface::getObjectClass
+     */
+    public function getObjectClass()
+    {
+        return $this->getEntityName();
+    }
+
+    /**
+     * @see RepositoryInterface::getObjectIdentifier
+     */
+    public function getObjectIdentifier()
+    {
+        return reset($this->getClassMetadata()->identifier);
+    }
+
+    public function createObjectInstance()
+    {
+        $className = $this->getObjectClass();
+        return new $className();
+    }
+}
