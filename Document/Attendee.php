e30db5cc673c880516d53a05f5cc8b27e9af7fea
Dylan Oliver <dylan@sweepingdesign.com>
2012-09-17 10:45:23 -0700

    add Managers for mongodb, fix mappings

---
+++ Document/Attendee.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rizza\CalendarBundle\Document;
+
+use Rizza\CalendarBundle\Model\Attendee as AbstractAttendee;
+
+class Attendee extends AbstractAttendee
+{
+}
---
+++ Document/EventManager.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Rizza\CalendarBundle\Document;
+
+use Rizza\CalendarBundle\Model\EventManager as BaseEventManager;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\DocumentRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Rizza\CalendarBundle\Model\EventInterface;
+use Rizza\CalendarBundle\Blamer\EventBlamerInterface;
+use Rizza\CalendarBundle\Model\CalendarInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class EventManager extends BaseEventManager
+{
+
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repo;
+
+    /**
+     * @var string
+     */
+    protected $class;
+
+    public function __construct(DocumentManager $em, $class)
+    {
+        $this->em = $em;
+        $this->repo = $em->getRepository($class);
+        $this->class = $class;
+    }
+
+    public function addEvent(EventInterface $event)
+    {
+        $this->em->persist($event);
+        $this->em->flush();
+    }
+
+    public function updateEvent(EventInterface $event)
+    {
+        $this->em->persist($event);
+        $this->em->flush();
+
+        return true;
+    }
+
+    public function removeEvent(EventInterface $event)
+    {
+        $this->em->remove($event);
+        $this->em->flush();
+
+        return true;
+    }
+    
+    public function find($id)
+    {
+        $event = $this->repo->find($id);
+        if (null === $event) {
+            throw new NotFoundHttpException(sprintf("Couldn't find event with id '%d'", $id));
+        }
+
+        return $event;
+    }
+
+    public function findAll()
+    {
+        return $this->repo->findAll();
+    }
+
+    public function findVisible($organizer)
+    {
+        if (!$organizer instanceof UserInterface) {
+          return array();
+        }
+        
+        $qb = $this->repo->createQueryBuilder()
+            ->field('organizer.$id')->equals(new \MongoId($organizer->getId()));
+//            ->field('visibility')->equals(CalendarInterface::VISIBILITY_PUBLIC);
+
+        return $qb->getQuery()->execute();
+    }
+
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    public function isAdmin($user, EventInterface $event)
+    {
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        return $user->equals($event->getOrganizer());
+    }
+
+}
